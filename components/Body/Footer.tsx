f3ff597fb53a2faf4e4d7a8f186d655cf33d7a53
zander404 <alexandrearthurpg@gmail.com>
2023-08-01 11:22:08 -0300

    .

--- components/Body/Footer.tsx
+++ components/Body/Footer.tsx
@@ -10,18 +10,18 @@ export const Footer = (props: FooterProps) => {
     return (
         <>
             <div className={"bg-[#2C2C2C] flex flex-row m-auto py-8 justify-between border-t-2 border-white"}>
-                <div id={"Logo"} className={"justify-center w-auto "}>
+                <div id={"Logo"} className={"justify-center pt-4 px-7 "}>
                     <Logo />
                 </div>
 
 
-                <div>
-                    <h1 className={"text-center w-auto "}>REDES SOCIAIS</h1>
+                <div className={"w-auto text-white"}>
+                    <h1 className={"text-center pb-2 "}>REDES SOCIAIS</h1>
                     <CardSociais />
                 </div>
 
 
-                <div id={"Créditos"} className={"text-white w-auto"}>
+                <div id={"Créditos"} className={"text-white w-auto pt-4 px-7"}>
                     <h1 className={"text-lg"}>Created by:</h1>
                     <h2 className={""} >Xandyy</h2>
 
